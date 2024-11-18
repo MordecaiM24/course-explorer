9c7fb7e7a331491ce183ba526ad3e47de5ee4c3a
Mordecai Mengesteab <mordecai.g.men@gmail.com>
2024-11-17 23:14:58 -0500

    edited coursescraper to start at top level of all courses. also started basic pipeline stuff

--- coursescraper/spiders/coursespider.py
+++ coursescraper/spiders/coursespider.py
@@ -1,4 +1,5 @@
 import scrapy
+from coursescraper.items import CourseItem
 
 
 class CoursespiderSpider(scrapy.Spider):
@@ -16,24 +17,33 @@ class CoursespiderSpider(scrapy.Spider):
         courses = response.css("div.courseblock")
         for course in courses:
 
-            code = course.css("div.cols .detail-coursecode strong::text").get()
+            # code = course.css("div.cols .detail-coursecode strong::text").get()
+            code_parts = course.css(
+                "div.cols .detail-coursecode strong::text, div.cols .detail-coursecode strong a::text"
+            ).getall()
+            code = " ".join(code_parts).strip()
+
             name = course.css("div.cols .detail-title strong::text").get()
             hours = course.css("div.cols .detail-hours_html::text").get()
 
+            # strip and rejoin b/c of a tags
             description_parts = course.css(
                 "div.noindent p.courseblockextra::text, div.noindent p.courseblockextra a::text"
             ).getall()
             description = " ".join(description_parts).strip()
 
+            # same thing here
             restrictions = course.css(
                 "p.courseblockextra.noindent a::text, p.courseblockextra.noindent::text"
             ).getall()
             restrictions_text = " ".join(restrictions).strip() if restrictions else None
 
-            yield {
-                "code": code,
-                "name": name,
-                "hours": hours,
-                "description": description,
-                "restrictions": restrictions_text,
-            }
+            course_item = CourseItem()
+
+            course_item["code"] = code
+            course_item["name"] = name
+            course_item["hours"] = hours
+            course_item["description"] = description
+            course_item["restrictions"] = restrictions_text
+
+            yield course_item
