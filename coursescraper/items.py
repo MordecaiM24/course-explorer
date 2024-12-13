1b4ee58ad62aba93d439597980ad14179a4f80c7
mordecai <mordecai.g.men@gmail.com>
2024-12-13 17:19:11 -0500

    edited coursescraper to start at top level of all courses. also started basic pipeline stuff

--- coursescraper/items.py
+++ coursescraper/items.py
@@ -6,7 +6,11 @@
 import scrapy
 
 
-class CoursescraperItem(scrapy.Item):
+class CourseItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    code = scrapy.Field()
+    name = scrapy.Field()
+    hours = scrapy.Field()
+    description = scrapy.Field()
+    restrictions = scrapy.Field()
--- coursescraper/pipelines.py
+++ coursescraper/pipelines.py
@@ -6,8 +6,30 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import re
 
 
 class CoursescraperPipeline:
     def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        field_names = adapter.field_names()
+
+        # remove non breaking spaces (mostly from course codes)
+        for field_name in field_names:
+            value = adapter.get(field_name)
+
+            if value is not None and isinstance(value, str):
+                adapter[field_name] = value.replace("\xa0", " ")
+
+        # get credit hours
+        for field_name in field_names:
+            if field_name is "hours":
+                hours = adapter.get(field_name)
+
+                if hours is not None:
+                    match = re.search(r"\(([\d-]+) credit hours\)", hours)
+                    if match:
+                        adapter[field_name] = match.group(1)
+
         return item
